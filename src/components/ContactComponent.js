ba935fd8634c34e8335e5713e1cbbce536868d67
Alan Renner <renner.131@osu.edu>
2021-01-19 17:35:42 -0800

    2021

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -22,39 +22,7 @@ class Contact extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      firstName: "",
-      lastName: "",
-      phoneNum: "",
-      email: "",
-      agree: false,
-      contactType: "By Phone",
-      feedback: "",
-      touched: {
-        firstName: false,
-        lastName: false,
-        phoneNum: false,
-        email: false,
-      },
-    };
-
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleBlur = (field) => () => {
-    this.setState({
-      touched: { ...this.state.touched, [field]: true },
-    });
-  };
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
   }
 
   handleSubmit(values) {
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,27 +1,27 @@
-import {createStore, combineReducers, applyMiddleware } from 'redux';
-import { createForms } from 'react-redux-form';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { Campsites } from './campsites';
-import { Comments } from './comments';
-import { Partners } from './partners';
-import { Promotions } from './promotions';
-import { InitialFeedback } from './forms';
-
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createForms } from "react-redux-form";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { Campsites } from "./campsites";
+import { Comments } from "./comments";
+import { Partners } from "./partners";
+import { Promotions } from "./promotions";
+import { InitialFeedback } from "./forms";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            campsites: Campsites,
-            comments: Comments,
-            partners: Partners,
-            promotions: Promotions,
-            ...createForms({
-                feedbackForm: InitialFeedback
-            })
-        }),
-        applyMiddleware(thunk,logger)
-    );
+  const store = createStore(
+    combineReducers({
+      campsites: Campsites,
+      comments: Comments,
+      partners: Partners,
+      promotions: Promotions,
+      ...createForms({
+        feedbackForm: InitialFeedback,
+      }),
+    }),
+    composeWithDevTools(applyMiddleware(thunk))
+  );
 
-    return store;
-}
+  return store;
+};
